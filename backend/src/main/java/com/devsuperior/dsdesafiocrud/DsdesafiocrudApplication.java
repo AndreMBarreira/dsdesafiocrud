2384fc44e8b2a5a4c38b97f35cf20a81899da4e9
AndreMBarreira <andremilla.am@gmail.com>
2022-08-05 13:55:46 -0300

    Start

---
+++ backend/src/main/java/com/devsuperior/dsdesafiocrud/DsdesafiocrudApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dsdesafiocrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsdesafiocrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsdesafiocrudApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/devsuperior/dsdesafiocrud/DsdesafiocrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dsdesafiocrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsdesafiocrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
